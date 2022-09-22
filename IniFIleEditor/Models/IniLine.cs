df8b33012799015da7480bbb6a5a0fc4be45bc60
jonnyrocket0 <wilda.jon@gmail.com>
2022-09-21 23:16:33 -0500

    cleanup

--- IniFIleEditor/Models/IniLine.cs
+++ IniFIleEditor/Models/IniLine.cs
@@ -8,11 +8,11 @@ namespace IniFIleEditor.Models
 {
     public class IniLine
     {
-        public string LineType { get; set; }
-        public string LineSubType { get; set; }
+        public string? LineType { get; set; }
+        public string? LineSubType { get; set; }
         public bool IsEnabled { get; set; } = true;
-        public string Definition { get; set; }
-        public string Target { get; set; }
+        public string? Definition { get; set; }
+        public string? Target { get; set; }
         public List<Conditional> Conditionals { get; set; } = new List<Conditional>();
     }
 }
